3fda8456cecdc1b440ab7adcfae9aa88eba9c2d7
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-17 21:25:01 -0500

    Extend User by adding the Name property

--- GigHub/Models/ApplicationUser.cs
+++ GigHub/Models/ApplicationUser.cs
@@ -9,6 +9,7 @@ namespace GigHub.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
