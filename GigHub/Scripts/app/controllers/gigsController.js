38352d9621c5a7ad846ea50f9a34f98fac3e2bc7
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-22 15:36:06 -0500

    Refactor javascript

---
+++ GigHub/Scripts/app/controllers/gigsController.js
@@ -0,0 +1,44 @@
+﻿var GigsController = function (attendanceService) {
+
+    var button;
+
+    var init = function(container) {
+        $(container).on("click", ".js-toggle-attendance", toggleAttendance);
+        $(container).on("click", ".js-toggle-follow", toggleFollow);
+    };
+
+    var toggleAttendance = function() {
+        button = $(e.target);
+
+        if (button.hasClass("btn-default"))
+            AttendanceService.createAttendance(button.attr("data-gig-id"), done, fail);
+        else
+            AttendanceService.deleteAttendance(button.attr("data-gig-id"), done, fail);
+    }
+
+    var toggleFollow = function() {
+        button = $(e.target);
+
+        $.post("/api/followings", { followeeId: button.attr("data-user-id") })
+            .done(function () {
+                button.text("Following");
+            })
+            .fail(function () {
+                alert("Something failed");
+            });
+    }
+
+    var done = function() {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+
+        button.toggleClass("btn-info").toggleClass("btn-default");
+    };
+
+    var fail = function() {
+        alert("Something failed.");
+
+    };
+    return {
+        init: init
+    }
+}(AttendanceService);
