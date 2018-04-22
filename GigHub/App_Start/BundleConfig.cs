765058418fa39faff6edc46b1afba525a8ca2ccb
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-21 21:55:57 -0500

    Use moment.js to format dates

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -9,6 +9,7 @@ namespace GigHub
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/moment-min.js",
                         "~/Scripts/underscore-min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
