34d882fbe657b6fcb774331b650bc050ffb76d33
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-23 22:41:12 -0500

    Refactor Gig and User data annotations using Fluent API

--- GigHub/Persistence/ApplicationDbContext.cs
+++ GigHub/Persistence/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
-﻿using System.Data.Entity;
-using GigHub.Core.Models;
+﻿using GigHub.Core.Models;
+using GigHub.Persistence.EntityConfigurations;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace GigHub.Persistence
 {
@@ -25,25 +26,8 @@ namespace GigHub.Persistence
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Attendance>()
-                .HasRequired(a => a.Gig)
-                .WithMany(g => g.Attendances)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<ApplicationUser>()
-                .HasMany(u => u.Followers)
-                .WithRequired(f => f.Followee)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<ApplicationUser>()
-                .HasMany(u => u.Followees)
-                .WithRequired(f => f.Follower)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<UserNotification>()
-                .HasRequired(n => n.User)
-                .WithMany(u => u.UserNotifications)
-                .WillCascadeOnDelete(false);
+            modelBuilder.Configurations.Add(new GigConfiguration());
+            modelBuilder.Configurations.Add(new UserConfiguration());
 
             base.OnModelCreating(modelBuilder);
         }
---
+++ GigHub/Persistence/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using GigHub.Core.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace GigHub.Persistence.EntityConfigurations
+{
+    public class UserConfiguration : EntityTypeConfiguration<ApplicationUser>
+    {
+        public UserConfiguration()
+        {
+            HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
+
+            HasMany(u => u.UserNotifications)
+                .WithRequired(un => un.User)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
