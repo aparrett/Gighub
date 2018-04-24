725403b58283f95b75f4f324b79c2cc5391a82a3
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-23 22:56:57 -0500

    Refactor data annotations to Fluent API on all other models

--- GigHub/Core/Models/Genre.cs
+++ GigHub/Core/Models/Genre.cs
@@ -1,13 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace GigHub.Core.Models
+﻿namespace GigHub.Core.Models
 {
     public class Genre
     {
         public int Id { get; set; }
-
-        [Required]
-        [StringLength(255)]
         public string Name { get; set; }
     }
 }
--- GigHub/Persistence/EntityConfigurations/UserConfiguration.cs
+++ GigHub/Persistence/EntityConfigurations/UserConfiguration.cs
@@ -7,16 +7,16 @@ namespace GigHub.Persistence.EntityConfigurations
     {
         public UserConfiguration()
         {
+            Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
             HasMany(u => u.Followers)
                 .WithRequired(f => f.Followee)
                 .WillCascadeOnDelete(false);
 
             HasMany(u => u.Followees)
                 .WithRequired(f => f.Follower)
-                .WillCascadeOnDelete(false);
-
-            HasMany(u => u.UserNotifications)
-                .WithRequired(un => un.User)
                 .WillCascadeOnDelete(false);
         }
     }
