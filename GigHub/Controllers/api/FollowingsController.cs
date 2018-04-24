c656985cd7c08ad462c1c6bf4e42836c5536dfd5
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-23 23:23:08 -0500

    Authorize Followings api

--- GigHub/Controllers/api/FollowingsController.cs
+++ GigHub/Controllers/api/FollowingsController.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 
 namespace GigHub.Controllers.api
 {
+    [Authorize]
     public class FollowingsController : ApiController
     {
         private readonly UnitOfWork _unitOfWork;
