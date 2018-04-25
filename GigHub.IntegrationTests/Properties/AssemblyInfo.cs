641a7ff663d2ba12c228d131e3ae2a2d62abd116
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-25 17:56:09 -0500

    Set up integration test project

---
+++ GigHub.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GigHub.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ee5b1cd-e367-4c44-9160-db36da0a9e13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GigHub.IntegrationTests/UnitTest2.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace GigHub.IntegrationTests
+{
+    [TestFixture]
+    public class UnitTest2
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var test = 1;
+            var result = 1;
+
+            Assert.That(result, Is.EqualTo(test));
+        }
+    }
+}
