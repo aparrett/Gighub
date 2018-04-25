6705b8299c19493dbf7fdaa69aabf8e0b3ef2c71
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-24 21:23:20 -0500

    Create first test method

--- GigHub.Tests/Controllers/Api/GigsControllerTests.cs
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -1,22 +1,36 @@
-﻿using GigHub.Controllers.api;
+﻿using FluentAssertions;
+using GigHub.Controllers.api;
 using GigHub.Core;
+using GigHub.Core.Repositories;
 using GigHub.Tests.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Web.Http.Results;
 
 namespace GigHub.Tests.Controllers.Api
 {
     [TestClass]
     public class GigsControllerTests
     {
-        private GigsController _controller;
+        private readonly GigsController _controller;
 
-        [TestMethod]
-        public void TestMethod1()
+        public GigsControllerTests()
         {
+            var mockRepository = new Mock<IGigRepository>();
+
             var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.SetupGet(u => u.Gigs).Returns(mockRepository.Object);
+
             _controller = new GigsController(mockUnitOfWork.Object);
             _controller.MockCurrentUser("1", "user1@domain.com");
+        }
+
+        [TestMethod]
+        public void Cancel_NoGigWithGivenIdExists_ShouldReturnNotFound()
+        {
+            var result = _controller.Cancel(1);
+
+            result.Should().BeOfType<NotFoundResult>();
         }
     }
 }
