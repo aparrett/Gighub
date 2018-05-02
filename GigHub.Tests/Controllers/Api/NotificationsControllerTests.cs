6f70302354384897bb77929f4fc59bc5fdde34dd
Anthony Parrett <anthonyparrett7@gmail.com>
2018-05-01 22:49:32 -0500

    Tests for AttendanceRepository.GetFutureAttendances

---
+++ GigHub.Tests/Controllers/Api/NotificationsControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using GigHub.Controllers.api;
+using GigHub.Core;
+using GigHub.Core.Repositories;
+using GigHub.Tests.Extensions;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace GigHub.Tests.Controllers.Api
+{
+    [TestFixture]
+    public class NotificationsControllerTests
+    {
+        private Mock<INotificationRepository> _mockRepository;
+        private NotificationsController _controller;
+        private string _userId;
+        private Mock<IUserNotificationRepository> _mockUserNotificationRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new Mock<INotificationRepository>();
+            _mockUserNotificationRepository = new Mock<IUserNotificationRepository>();
+
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.SetupGet(u => u.Notifications).Returns(_mockRepository.Object);
+            mockUnitOfWork.SetupGet(u => u.UserNotifications).Returns(_mockUserNotificationRepository.Object);
+
+            _controller = new NotificationsController(mockUnitOfWork.Object);
+            _userId = "1";
+            _controller.MockCurrentUser(_userId, "user1@domain.com");
+        }
+    }
+}
---
+++ GigHub.Tests/Persistence/Repositories/AttendanceRepositoryTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using GigHub.Core.Models;
+using GigHub.Persistence;
+using GigHub.Persistence.Repositories;
+using GigHub.Tests.Extensions;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Data.Entity;
+
+namespace GigHub.Tests.Persistence.Repositories
+{
+    [TestFixture]
+    public class AttendanceRepositoryTests
+    {
+        private AttendanceRepository _repository;
+        private Mock<DbSet<Attendance>> _mockAttendances;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockAttendances = new Mock<DbSet<Attendance>>();
+
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.Attendances).Returns(_mockAttendances.Object);
+
+            _repository = new AttendanceRepository(mockContext.Object);
+        }
+
+        [Test]
+        public void GetFutureAttendances_GigIsInThePast_ShouldNotBeReturned()
+        {
+            var gig = new Gig { DateTime = DateTime.Now.AddDays(-1), ArtistId = "1" };
+            var attendance = new Attendance { AttendeeId = "1", Gig = gig };
+
+            _mockAttendances.SetSource(new[] { attendance });
+
+            var attendances = _repository.GetFutureAttendances("1");
+
+            attendances.Should().BeEmpty();
+        }
+
+        [Test]
+        public void GetFutureAttendances_GigForDifferentUser_ShouldNotBeReturned()
+        {
+            var gig = new Gig { DateTime = DateTime.Now.AddDays(-1), ArtistId = "1" };
+            var attendance = new Attendance { AttendeeId = "1", Gig = gig };
+
+            _mockAttendances.SetSource(new[] { attendance });
+
+            var attendances = _repository.GetFutureAttendances("2");
+
+            attendances.Should().BeEmpty();
+        }
+
+        [Test]
+        public void GetFutureAttendance_GigInFutureForGivenUser_ShouldBeReturned()
+        {
+            var gig = new Gig { DateTime = DateTime.Now.AddDays(1), ArtistId = "1" };
+            var attendance = new Attendance { AttendeeId = "1", Gig = gig };
+
+            _mockAttendances.SetSource(new[] { attendance });
+
+            var attendances = _repository.GetFutureAttendances("1");
+
+            attendances.Should().Contain(attendance);
+        }
+    }
+}
