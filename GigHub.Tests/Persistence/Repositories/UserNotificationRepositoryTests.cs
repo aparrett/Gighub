5bb4f7fb0769e6619b0a2ba88389ffcf64bf8d2d
Anthony Parrett <anthonyparrett7@gmail.com>
2018-05-03 23:58:36 -0500

    Set up UserNotificationRepositoryTests

---
+++ GigHub.Tests/Persistence/Repositories/UserNotificationRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using GigHub.Core.Models;
+using GigHub.Persistence;
+using GigHub.Persistence.Repositories;
+using Moq;
+using NUnit.Framework;
+using System.Data.Entity;
+
+namespace GigHub.Tests.Persistence.Repositories
+{
+    [TestFixture]
+    public class UserNotificationRepositoryTests
+    {
+        private UserNotificationRepository _repository;
+        private Mock<DbSet<UserNotification>> _mockUserNotifications;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockUserNotifications = new Mock<DbSet<UserNotification>>();
+
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.UserNotifications).Returns(_mockUserNotifications.Object);
+
+            _repository = new UserNotificationRepository(mockContext.Object);
+        }
+
+        [Test]
+        public void GetUnread()
+        {
+        }
+    }
+}
