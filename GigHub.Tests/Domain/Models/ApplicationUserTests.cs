d0169486fadb24f64622278aa905d084dd6d6d90
Anthony Parrett <anthonyparrett7@gmail.com>
2018-05-04 21:23:58 -0500

    Add  Tests for ApplicationUser.Notify

---
+++ GigHub.Tests/Domain/Models/ApplicationUserTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using GigHub.Core.Models;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System.Linq;
+
+namespace GigHub.Tests.Domain.Models
+{
+    [TestFixture]
+    public class ApplicationUserTests
+    {
+        [Test]
+        public void Notify_WhenCalled_ShouldAddTheNotification()
+        {
+            var user = new ApplicationUser();
+            var notification = Notification.GigCancelled(new Gig());
+
+            user.Notify(notification);
+
+            user.UserNotifications.Count.Should().Be(1);
+
+            var userNotification = user.UserNotifications.First();
+            userNotification.Notification.Should().Be(notification);
+            userNotification.User.Should().Be(user);
+        }
+    }
+}
